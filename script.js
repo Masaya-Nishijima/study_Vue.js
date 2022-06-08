ca95457355111272e071334699fdee34f594bd18
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-08 15:23:20 +0900

    feat: メモの削除機能を追加

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ var todo = new Vue({
       { text: 'memo 1' },
       { text: 'memo 2' }
     ]
+  },
+  methods: {
+    destoryTodo: function (todo_index) {
+      this.todos.splice(todo_index, 1)
+    }
   }
 })
