55d4dc293b89978bcc875c3bb6cb654611b868a2
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-09 09:37:10 +0900

    refactor: JavaScript Standard Style

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-var todo = new Vue({
+const todo = new Vue({
   el: '#todoItem',
   data: {
-    todos:[
+    todos: [
       { text: 'memo 1', edit: true },
       { text: 'memo 2', edit: false }
     ],
     new_todo: 'new todo'
   },
   methods: {
-    destroyTodo: function (todo_index) {
-      this.todos.splice(todo_index, 1)
+    destroyTodo: function (todoIndex) {
+      this.todos.splice(todoIndex, 1)
     },
     createTodo: function () {
-      this.todos.push({text: this.new_todo, edit: false})
+      this.todos.push({ text: this.new_todo, edit: false })
       this.new_todo = 'new todo'
     },
-    changeMode: function (todo_index) {
-      this.todos[todo_index].edit = !this.todos[todo_index].edit
+    changeMode: function (todoIndex) {
+      this.todos[todoIndex].edit = !this.todos[todoIndex].edit
     },
     exportTodos: function () {
       localStorage.clear()
@@ -27,7 +27,7 @@ var todo = new Vue({
     importTodos: function () {
       this.todos = []
       for (let index = 0; index < localStorage.length; index++) {
-        let todo = {
+        const todo = {
           text: localStorage.getItem(localStorage.key(index)),
           edit: false
         }
