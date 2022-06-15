8c6794def010e309528a4c74849b351adfd9c469
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-06-15 13:50:33 +0900

    refactor: 不要になっていたimportコマンドを削除

--- script.js
+++ script.js
@@ -28,16 +28,6 @@ const todo = new Vue({
       this.todos[todoIndex].edit = !this.todos[todoIndex].edit
       this.exportTodos()
     },
-    importTodos() {
-      this.todos = []
-      for (let index = 0; index < localStorage.length; index++) {
-        const todo = {
-          text: localStorage.getItem(localStorage.key(index)),
-          edit: false
-        }
-        this.todos.push(todo)
-      }
-    },
     exportTodos() {
       localStorage.clear()
       this.todos.forEach((todo, index) => {
